261b0f07b4e20aaa189b61bea3286bce2ff4d4b6
gargdivya07 <42088741+gargdivya07@users.noreply.github.com>
2019-04-22 16:21:03 +0530

    Update EventCreationDetailsParser.java

--- backend/EventPlanning/src/main/java/spring/event/model/EventCreationDetailsParser.java
+++ backend/EventPlanning/src/main/java/spring/event/model/EventCreationDetailsParser.java
@@ -56,46 +56,76 @@ public class EventCreationDetailsParser {
 	public String getEventtype() {
 		return eventtype;
 	}
-
+        
+	public void setEventtype(String eventtype) {
+		this.eventtype=eventtype;
+	}
 
 	public String getEventname() {
 		return eventname;
 	}
-
+       
+	public void setEventname(String eventname) {
+		this.eventname=eventname;
+	}
 
 	public String getEventdate() {
 		return eventdate;
+	}
+	
+	public void setEventdate(String eventdate) {
+		this.eventdate=eventdate;
 	}
 
 
 	public String getDescription() {
 		return description;
 	}
+	
+	public void setDescription(String description) {
+		this.description=description;
+	}
 
 
 	public int getParticipantcount() {
 		return participantcount;
+	}
+	
+	public void setParticipantcount(int participantcount) {
+		this.participantcount=participantcount;
 	}
 
 
 	public String getEventlocation() {
 		return eventlocation;
 	}
+	
+	public void setEventlocation(String eventlocation) {
+		this.eventlocation=eventlocation;
+	}
 
 
 	public int getEventfees() {
 		return register_fee;
+	}
+       
+	public void setEventfees(int register_fee) {
+	    this.register_fee=register_fee;
 	}
 
 
 	public long getOrganizerid() {
 		return organizer_id;
 	}
-
+         public void setOrganizerid(long organizer_id) {
+		this.organizer_id=organizer_id;
+	}
 
 	public String getLastdate() {
 		return last_date;
 	}
-	
+	public void setLastdate() {
+		this.last_date=last_date;
+	}
 	
 }
