711907b0ed134e44465b34d98023bec75c9d1ba1
Parul Parikh <41738754+ParulParikh3@users.noreply.github.com>
2019-04-24 14:22:23 +0530

    Test added

--- backend/EventPlanning/src/test/java/spring/event/EventPlanningApplicationTests.java
+++ backend/EventPlanning/src/test/java/spring/event/EventPlanningApplicationTests.java
@@ -1,20 +1,38 @@
 package spring.event;
 
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class EventPlanningApplicationTests {
+import spring.event.model.Event;
+import spring.event.repository.EventRepository;
 
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class EventPlanningApplicationTests {
+	
+	@Autowired
+	ApplicationContext context;
 	@Test
-	public void contextLoads() {
+	public void contextLoads() {   
+		EventRepository eventrepository=context.getBean(EventRepository.class);
+	
 		
-		System.out.println("******************hi*****************");
+		List<Event> list=(List<Event>)eventrepository.findAll();
+		for(int i=0;i<list.size();i++)
+		{
+			System.out.println(list.get(i).getEventname());
+		}
+		
+			
 	}
-	
-	
 
 }
